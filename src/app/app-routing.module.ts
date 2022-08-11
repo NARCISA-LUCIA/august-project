cdfe866bc65b0f19bd32f857c2ca7c0c82f96d59
Narcisa <rusnarcisa16@gmail.com>
2022-08-11 13:50:09 +0300

    Create REST API for Project (GET, POST, UPDATE, DELETE)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,18 @@ import { RouterModule, Routes } from '@angular/router';
 import {CreateCompanyComponent} from "./company-page/create-company/create-company.component";
 import {UpdateCompanyComponent} from "./company-page/update-company/update-company.component";
 import {CompanyPageComponent} from "./company-page/company-page.component";
+import {ProjectPageComponent} from "./project-page/project-page.component";
+import {CreateProjectComponent} from "./project-page/create-project/create-project.component";
+import {UpdateProjectComponent} from "./project-page/update-project/update-project.component";
 
 const routes: Routes = [
   {path:"company/create", component:CreateCompanyComponent},
   {path:"company/:id/update", component:UpdateCompanyComponent},
   {path:"companies", component:CompanyPageComponent},
-];
+  {path:"projects", component:ProjectPageComponent},
+  {path:"project/create", component:CreateProjectComponent},
+  {path:"project/:id/update", component:UpdateProjectComponent},
+ ]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/model/project.ts
@@ -0,0 +1,8 @@
+import {Company} from "./company";
+
+export class Project {
+    id: number;
+    name: string;
+    description: string;
+    company: Company;
+}
